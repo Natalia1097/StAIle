41d66601e0ac4d19b9e9453daced4f65fb313bd6
Natalia1097 <162326314+Natalia1097@users.noreply.github.com>
2026-06-11 13:38:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 import streamlit as st
 import json
+import numpy as np
+from PIL import Image
 
 st.set_page_config(
     page_title="StAIle",
-    page_icon="👔",
-    layout="centered"
+    page_icon="👔"
 )
 
 st.title("👔 StAIle")
 st.subheader("IA para Combinação de Cores de Roupas")
 
+# Carregar dataset
 with open("colors.json", "r", encoding="utf-8") as f:
     cores_dataset = json.load(f)
 
 st.success(f"Dataset carregado: {len(cores_dataset)} cores")
+
+# Função para encontrar a cor mais próxima
+def encontrar_cor_mais_proxima(rgb_detectado):
+
+    menor_distancia = float("inf")
+    melhor_cor = None
+
+    for cor in cores_dataset:
+
+        rgb_dataset = np.array(cor["rgb"])
+
+        distancia = np.linalg.norm(
+            np.array(rgb_detectado) - rgb_dataset
+        )
+
+        if distancia < menor_distancia:
+            menor_distancia = distancia
+            melhor_cor = cor
+
+    return melhor_cor
+
 
 imagem = st.file_uploader(
     "Envie uma foto da roupa",
@@ -21,5 +44,32 @@ imagem = st.file_uploader(
 )
 
 if imagem:
-    st.image(imagem, caption="Imagem enviada")
-    st.info("Próxima etapa: detectar cor da roupa")
+
+    img = Image.open(imagem)
+
+    st.image(img, caption="Imagem enviada")
+
+    # Reduz tamanho para acelerar
+    img = img.resize((100, 100))
+
+    pixels = np.array(img)
+
+    # Média das cores da imagem
+    rgb = pixels.mean(axis=(0, 1))
+
+    rgb = rgb.astype(int)
+
+    st.write("RGB detectado:")
+    st.write(rgb)
+
+    cor_encontrada = encontrar_cor_mais_proxima(rgb)
+
+    st.subheader("Cor encontrada")
+
+    st.success(cor_encontrada["name"])
+
+    st.write("HEX:")
+    st.code(cor_encontrada["hex"])
+
+    st.write("RGB:")
+    st.write(cor_encontrada["rgb"])
